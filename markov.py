a32a3d0cfcf264014973362f3507d7c2bd0f4f29
kevinluk1 <lukk@oregonstate.edu>
2023-08-13 18:00:21 -0700

    Update markov.py

--- markov.py
+++ markov.py
@@ -29,8 +29,8 @@ def map_word_to_word(corpus : list) -> dict:  # one word markov model
             suffix = corpus[index + 1]
             dict1_to_1[key].append(suffix)
 
-    logging.debug("map_word_to_word results for \"sake\" = %s\n",
-                  dict1_to_1['sake'])
+
+    logging.debug(f'map_words_to_word_results for "sake" = \n {dict1_to_1["sake"]}')
 
     return dict1_to_1
 
@@ -44,8 +44,8 @@ def map_2_words_to_word(corpus: list) -> dict:  # two word markov model
             suffix = corpus[index + 2]
             dict2_to_1[key].append(suffix)
 
-    logging.debug("map_2_words_to_word results for \"sake jug\" = %s\n",
-                  dict2_to_1['sake jug'])
+
+    logging.debug(f'map_2_words_to_word results for "sake jug" = \n {dict2_to_1["sake jug"]}')
     return dict2_to_1
 
 
@@ -56,3 +56,62 @@ def seed_word(corpus: list) -> tuple:
         return (word, num_sylls)
     else:
         return seed_word(corpus)
+
+
+
+def word_after_single(prefix, markov1, current_sylls, target_sylls) -> list:
+    accepted_words = []
+    suffixes = markov1.get(prefix)
+    if suffixes is not None:
+        for candidate in suffixes:
+            num_sylls = count_syllables(candidate)
+            if current_sylls + num_sylls <= target_sylls:
+                accepted_words.append(candidate)
+
+    logging.debug(f'accepted words after "{prefix}" = {set(accepted_words)}\n')
+
+    return accepted_words
+
+
+def word_after_double(prefix, markov2, current_sylls, target_sylls) -> list:
+    accepted_words = []
+    suffixes = markov2.get(prefix)
+    if suffixes is not None:
+        for candidate in suffixes:
+            num_sylls = count_syllables(candidate)
+            if current_sylls + num_sylls <= target_sylls:
+                accepted_words.append(candidate)
+
+    logging.debug(f'accepted words after "{prefix}" = {set(accepted_words)}\n')
+    return accepted_words
+
+
+def haiku_line(markov1, markov2, corpus, end_prev_line, target_sylls) -> tuple:
+    line = '2 or 3'
+    line_sylls = 0
+    current_line = []
+    if len(end_prev_line) == 0:  # on line 1
+        line = '1'
+        word, num_sylls = seed_word(corpus)
+        current_line.append(word)
+        line_sylls += num_sylls
+        word_choices: list = word_after_single(word, markov1, line_sylls, target_sylls)
+        word: str = random.choice(word_choices)
+
+        while len(word_choices) == 0:   # ghost prefix, not added to the line, just used to re-access markov model
+            backup_prefix = random.choice(corpus)
+            logging.debug(f'new random prefix = "{backup_prefix}"')
+            word_choices: list = word_after_single(backup_prefix, markov1, line_sylls, target_sylls)
+
+
+        word = random.choice(word_choices)
+        num_sylls: int = count_syllables(word)
+        logging.debug(f'"word and syllables = {word},{num_sylls}"')
+
+        line_sylls += num_sylls
+        current_line.append(word)
+        if line_sylls == target_sylls:
+            end_prev_line.extend(current_line[-2:])  # prep to use 2nd order markov chain
+            return (current_line, end_prev_line)  # pass the end of the prev line to use as markov chain key
+
+
